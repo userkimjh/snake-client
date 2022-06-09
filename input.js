0329034c113427c24e0bcdfc73108bb7b1cb9441
John Kim <96427720+userkimjh@users.noreply.github.com>
2022-06-09 19:38:49 -0400

    refactor play and input file

---
+++ input.js
@@ -0,0 +1,21 @@
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = (input) => {
+  if(input === '\u0003') {
+    console.log('exited');
+    process.exit();
+  }
+};
+
+module.exports = {setupInput};
--- play.js
+++ play.js
@@ -1,26 +1,7 @@
-const connect = require('./client');
+const {connect} = require('./client');
+const {setupInput} = require('./input');
 
 console.log("Connecting ...");
 connect();
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-const handleUserInput = (input) => {
-  if(input === '\u0003') {
-    console.log('exited');
-    process.exit();
-  }
-};
 
 setupInput();
