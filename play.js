76a16266d975343a3d6c56f997c26593a15ac259
John Kim <96427720+userkimjh@users.noreply.github.com>
2022-06-09 12:59:05 -0400

    function to exit game

--- play.js
+++ play.js
@@ -2,3 +2,25 @@ const connect = require('./client');
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = (input) => {
+  if(input === '\u0003') {
+    console.log('exited');
+    process.exit();
+  }
+};
+
+setupInput();
